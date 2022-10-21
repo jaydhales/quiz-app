a9183fbeddf2fa9c9270d8c81c60e22360b40199
James Osunkiyesi <jaydhales@gmail.com>
2022-10-21 20:44:02 +0100

    added data to array

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const questions = [
   {
-    question: "What is your name",
-    optionA: "Jay",
-    optionB: "Joe",
-    optionC: "Jill",
+    question: "Neutral component with pH range 7 represents what color?",
+    optionA: "Blue",
+    optionB: "Yellow",
+    optionC: "Green",
+    correctOpt: "optionC",
+  },
+  {
+    question: "Belarus is located in which part of Europe?",
+    optionA: "Northern",
+    optionB: "Eastern",
+    optionC: "Southern",
+    correctOpt: "optionB",
+  },
+  {
+    question: "Which of the following is NOT a Saudi Arabian brand?",
+    optionA: "Gazprom",
+    optionB: "ADNOC",
+    optionC: "Sberbank",
+    correctOpt: "optionB",
+  },
+  {
+    question: "What is the most common fuel used in nuclear reactor?",
+    optionA: "Uranium",
+    optionB: "Mineral Salt",
+    optionC: "Hydrogen",
+    correctOpt: "optionA",
+  },
+  {
+    question: "What is the most famous port?",
+    optionA: "Port of Shanghai",
+    optionB: "Port of Hong Kong",
+    optionC: "Port of Singapore",
+    correctOpt: "optionA",
+  },
+  {
+    question: "What is a 2 line poem?",
+    optionA: "Amalthea",
+    optionB: "Couplets",
+    optionC: "Callisto",
     correctOpt: "optionB",
   },
   {
@@ -14,11 +49,61 @@ const questions = [
     correctOpt: "optionB",
   },
   {
-    question: "What is your name",
-    optionA: "Jay",
-    optionB: "Joe",
-    optionC: "Jill",
+    question:
+      "In which film, Arnold Schwarzenegger acts as the Howard Langston?",
+    optionA: "Staying Hungry",
+    optionB: "Pumping Iron",
+    optionC: "Jingle All the Way",
+    correctOpt: "optionC",
+  },
+  {
+    question: "People in Chile speak in",
+    optionA: "English",
+    optionB: "Spanish",
+    optionC: "French",
     correctOpt: "optionB",
+  },
+  {
+    question: "Which of the following is a product brand from Mexico?",
+    optionA: "Malee",
+    optionB: "Atlantis",
+    optionC: "Atletica",
+    correctOpt: "optionC",
+  },
+  {
+    question: "What is the least populated country in South America?",
+    optionA: "Suriname",
+    optionB: "Mexico",
+    optionC: "Venezuela",
+    correctOpt: "optionA",
+  },
+  {
+    question: "Ukraine is located in which part of Europe?",
+    optionA: "Southern",
+    optionB: "Eastern",
+    optionC: "Western",
+    correctOpt: "optionB",
+  },
+  {
+    question: "What is the UN Code for Cyprus?",
+    optionA: "196",
+    optionB: "148",
+    optionC: "156",
+    correctOpt: "optionA",
+  },
+  {
+    question: "Which country has the oldest harbor?",
+    optionA: "Italy",
+    optionB: "Ukraine",
+    optionC: "Egypt",
+    correctOpt: "optionC",
+  },
+  {
+    question: "Which of the following is a European country?",
+    optionA: "Togo",
+    optionB: "Tunisia",
+    optionC: "Lithuania",
+    correctOpt: "optionc",
   },
 ];
 
@@ -28,7 +113,7 @@ const resultDiv = document.querySelector("#result");
 const questionForm = document.querySelector("#question-form");
 const questionBox = document.querySelector("#question-box");
 const options = document.querySelectorAll(".options");
-const prevBtn = document.querySelector("#prev-btn");
+// const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const submitBtn = document.querySelector("#submit-btn");
 
@@ -51,11 +136,11 @@ const updateDom = () => {
   });
 
   //   toggle previous, next and submit button
-  if (questionIndex === 0) {
-    prevBtn.disabled = true;
-  } else {
-    prevBtn.disabled = false;
-  }
+  //   if (questionIndex === 0) {
+  //     prevBtn.disabled = true;
+  //   } else {
+  //     prevBtn.disabled = false;
+  //   }
 
   if (questionIndex < questions.length - 1) {
     submitBtn.disabled = true;
@@ -84,9 +169,9 @@ const runEvents = (target, action) => {
     updateDom();
   });
 };
-runEvents(prevBtn, () => {
-  questionIndex > 0 && questionIndex--;
-});
+// runEvents(prevBtn, () => {
+//   questionIndex > 0 && questionIndex--;
+// });
 runEvents(nextBtn, () => {
   questionIndex < questions.length - 1 && questionIndex++;
 });
